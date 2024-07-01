8e6a50e1663841003d67fd8ca5609929502ad3c9
Daria <haragozan@gmail.com>
2024-07-01 12:06:06 +0300

    Footer

--- Masha_shop/base/components/modules/Footer/Footer.tsx
+++ Masha_shop/base/components/modules/Footer/Footer.tsx
@@ -1,35 +1,50 @@
 import React from 'react';
+import Logo from '@/components/elements/Logo/Logo';
 
 const Footer = () => {
-    return (
-        <footer>
-            <div className="footer-container">
-                <div className="footer-column">
-                    <h3>Nuestras redes</h3>
-                    <ul>
-                        <li><a href="#">Instagram</a></li>
-                        <li><a href="#">TikTok</a></li>
-                        <li><a href="#">Facebook</a></li>
-                    </ul>
-                </div>
-                <div className="footer-column">
-                    <h3>Atención al cliente</h3>
-                    <ul>
-                        <li><a href="#">Formas de pago</a></li>
-                        <li><a href="#">Cambios y devoluciones</a></li>
-                        <li><a href="#">Métodos de Envío</a></li>
-                        <li><a href="#">Guía de tallas</a></li>
-                    </ul>
-                </div>
-                <div className="footer-column">
-                    <h3>Libro de Reclamaciones</h3>
-                    <ul>
-                        <li><a href="#">Hacer una reclamación</a></li>
-                    </ul>
-                </div>
-            </div>
-        </footer>
-    );
-}
+  return (
+    <footer className="footer">
+      <div className="footer__top">
+        <div className="container footer__top__container">
+          <div className="footer__logo">
+            <Logo />
+          </div>
+          <div className="footer__socials">
+          <div className="footer__socials__title">Nuestras redes</div>
+            <ul className="list-reset footer__socials__list">
+              <li className="footer__socials__item">
+                <a
+                  href="https://www.instagram.com/polina__store?igsh=emRkMWJ2aDR4MTA4"
+                  className="footer__socials__item__link"
+                >
+                  <div className="footer__socials__item__link__icon" />
+                </a>
+              </li>
+              <li className="footer__socials__item">
+                <a
+                  href="https://www.tiktok.com/"
+                  className="footer__socials__item__link"
+                >
+                  <div className="footer__socials__item__link__icon" />
+                </a>
+              </li>
+              <li className="footer__socials__item">
+                <a
+                  href="https://www.facebook.com/share/jK659SCv5LneWj4e/?mibextid=LQQJ4d"
+                  className="footer__socials__item__link"
+                >
+                  <div className="footer__socials__item__link__icon" />
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="footer__image">
+            <img src="/img/book.png" alt="Book" />
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
